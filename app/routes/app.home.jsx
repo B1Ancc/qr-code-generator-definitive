72172423ce416dd0bdc0c9a765521e7407f40d76
REXV <bagaming40@gmail.com>
2025-07-31 17:22:58 +0700

    changed how the home layout works (25w5mJLa)

--- app/routes/app.home.jsx
+++ app/routes/app.home.jsx
@@ -1,4 +1,4 @@
-import { Card, DataTable, FooterHelp, Page, Text } from "@shopify/polaris";
+import { BlockStack, Card, DataTable, Divider, FooterHelp, Grid, InlineStack, Link, Page, Text } from "@shopify/polaris";
 import Footer from "./components/Footer";
 import { authenticate } from "../shopify.server";
 import { useLoaderData } from "@remix-run/react";
@@ -9,11 +9,15 @@ export const loader = async ({ request }) => {
     const { shop } = session;
     const data = await db.qRCode.findMany(
         {
+            take: 5,
+            orderBy: {
+                createdAt: 'desc'
+            },
             where: { shop },
             select: {
                 title: true,
-                destination: true,
-                pattern: true,
+                endpoint: true,
+                type: true,
                 createdAt: true,
             }
         }
@@ -23,33 +27,61 @@ export const loader = async ({ request }) => {
 
 export default function Home() {
     const rows = useLoaderData();
-    const row = rows.map((single) => [single.title, single.destination, single.pattern, single.createdAt]);
+    const row = rows.map(({ title, endpoint, type, createdAt }) => {
+        const date = new Date(createdAt);
+        const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+
+        return [title, endpoint, type, formattedDate];
+    });
 
     return (
         <>
             <Page title="QR Codes Generator Definitive" subtitle="Create a QR code instantly. Access them whenever you want, wherever you go.">
-                <Card>
-                    <Text variant="headingMd" as="h6">
-                        Recent created QR codes
-                    </Text>
-                    <DataTable
-                        columnContentTypes={[
-                            'text',
-                            'text',
-                            'text',
-                            'text'
-                        ]}
-                        headings={[
-                            'Name',
-                            'Destination',
-                            'Type',
-                            'Scans',
-                        ]}
+                <BlockStack gap="200">
+                    <Card>
+                        <Text variant="headingMd" as="h6">
+                            Recent created QR codes
+                        </Text>
+                        <DataTable
+                            columnContentTypes={[
+                                'text',
+                                'text',
+                                'text',
+                                'text'
+                            ]}
+                            headings={[
+                                'Name',
+                                'Endpoint',
+                                'Type',
+                                'Created at',
+                            ]}
 
-                        rows={row}
-                    >
-                    </DataTable>
-                </Card>
+                            rows={row}
+                        >
+                        </DataTable>
+                    </Card>
+                    <Grid columns={3}>
+                        <Grid.Cell columnSpan={{ xs: 4, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                            <Card>
+                                <InlineStack align="center">
+                                    <img src="/assets/qr-type/dots.png" width="100%" height="100%" objectFit="contain"></img>
+                                    <Divider />
+                                    <Link url="/app/static">Create a QR code</Link>
+                                </InlineStack>
+                            </Card>
+                        </Grid.Cell>
+                        <Grid.Cell columnSpan={{ xs: 4, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                            <Card>
+                                <Link url="/app/list">See your QR codes</Link>
+                            </Card>
+                        </Grid.Cell>
+                        <Grid.Cell columnSpan={{ xs: 4, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                            <Card>
+                                <Link url="/app/settings">Settings</Link>
+                            </Card>
+                        </Grid.Cell>
+                    </Grid>
+                </BlockStack>
                 <Footer />
             </Page>
         </>
--- app/routes/components/Homepage.jsx
+++ app/routes/components/Homepage.jsx
@@ -4,10 +4,12 @@ import {
 import QRSettings from "./QRSettings";
 import QRCustomizations from "./QRCustomizations";
 import QRTarget from "./QRTarget";
+import GlobalSaveBar from "./GlobalSaveBar";
 
 export default function Homepage({ qrData }) {
     return (
         <BlockStack gap="200">
+            <GlobalSaveBar saveData={qrData}/>
             <QRTarget />
             <QRSettings settingsData={qrData} />
             <QRCustomizations customData={qrData} />
